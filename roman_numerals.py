ea2d83a9dd3030871d715d43b0b3422408cc27c7
Memdved <149522849+Memdved@users.noreply.github.com>
2025-01-11 23:54:57 +0300

    Added possibility of mathematical operations with string Roman numerals.

--- roman_numerals.py
+++ roman_numerals.py
@@ -29,6 +29,15 @@ class BaseRomanNumeral(abc.ABC):
         """
 
     @abc.abstractmethod
+    def roman_to_int(self, roman: str) -> int:
+        """Method to convert a Roman numeral to a number.
+        :param roman: Roman numeral to convert.
+        :type roman: str
+        :return: Number.
+        :rtype: int
+        """
+
+    @abc.abstractmethod
     def __add__(self, other: "BaseRomanNumeral") -> "BaseRomanNumeral":
         pass
 
@@ -56,15 +65,45 @@ class BaseRomanNumeral(abc.ABC):
 class RomanNumeral(BaseRomanNumeral):
     """Roman numeral class."""
 
-    def __init__(self, int_representation: int) -> None:
-        self.__int_representation = int_representation
-        self.__roman_num = self.int_to_roman(self.__int_representation)
+    def __init__(self, representation: int | str) -> None:
+        if isinstance(representation, int):
+            self.__int_representation = representation
+            self.__roman_num = self.int_to_roman(self.__int_representation)
+        else:
+            self.__roman_num = representation
+            self.__int_representation = self.roman_to_int(self.__roman_num)
 
     def __int__(self) -> int:
         return self.__int_representation
 
     def __str__(self) -> str:
         return self.__roman_num
+
+    def roman_to_int(self, roman: str) -> int:
+        """Метод для преобразования римской цифры в число.
+
+        :param roman: Римская цифра для преобразования.
+        :type roman: str
+        :return: Число.
+        :rtype: int
+        """
+        roman_to_int_map = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        int_val = 0
+        for i, char in enumerate(roman):
+            if i > 0 and roman_to_int_map[char] > roman_to_int_map[roman[i - 1]]:
+                int_val += roman_to_int_map[char] - 2 * roman_to_int_map[roman[i - 1]]
+            else:
+                int_val += roman_to_int_map[char]
+        return int_val
+
 
     def int_to_roman(self, num: int) -> str:
         val: list[int] = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
@@ -85,28 +124,40 @@ class RomanNumeral(BaseRomanNumeral):
         ]
 
         roman_num: str = ""
-        i = 0
+        iteration: int = 0
         while num > 0:
-            for _ in range(num // val[i]):
-                roman_num += syb[i]
-                num -= val[i]
-            i += 1
+            for _ in range(num // val[iteration]):
+                roman_num += syb[iteration]
+                num -= val[iteration]
+            iteration += 1
         return roman_num
 
-    def __add__(self, other: "BaseRomanNumeral") -> "BaseRomanNumeral":
-        return RomanNumeral(self.__int_representation + int(other))
+    def __add__(self, other):
+        if isinstance(other, RomanNumeral):
+            return RomanNumeral(self.__int_representation + int(other))
+        return RomanNumeral(self.__int_representation + int(RomanNumeral(other)))
 
-    def __sub__(self, other: "BaseRomanNumeral") -> "BaseRomanNumeral":
-        return RomanNumeral(self.__int_representation - int(other))
+    def __sub__(self, other):
+        if isinstance(other, RomanNumeral):
+            return RomanNumeral(self.__int_representation - int(other))
+        return RomanNumeral(self.__int_representation - int(RomanNumeral(other)))
 
-    def __mul__(self, other: "BaseRomanNumeral") -> "BaseRomanNumeral":
-        return RomanNumeral(self.__int_representation * int(other))
+    def __mul__(self, other):
+        if isinstance(other, RomanNumeral):
+            return RomanNumeral(self.__int_representation * int(other))
+        return RomanNumeral(self.__int_representation * int(RomanNumeral(other)))
 
-    def __truediv__(self, other: "BaseRomanNumeral") -> "BaseRomanNumeral":
-        return RomanNumeral(self.__int_representation // int(other))
+    def __truediv__(self, other):
+        if isinstance(other, RomanNumeral):
+            return RomanNumeral(self.__int_representation // int(other))
+        return RomanNumeral(self.__int_representation // int(RomanNumeral(other)))
 
-    def __floordiv__(self, other: "BaseRomanNumeral") -> "BaseRomanNumeral":
-        return RomanNumeral(self.__int_representation // int(other))
+    def __floordiv__(self, other):
+        if isinstance(other, RomanNumeral):
+            return RomanNumeral(self.__int_representation // int(other))
+        return RomanNumeral(self.__int_representation // int(RomanNumeral(other)))
 
-    def __mod__(self, other: "BaseRomanNumeral") -> "BaseRomanNumeral":
-        return RomanNumeral(self.__int_representation % int(other))
+    def __mod__(self, other):
+        if isinstance(other, RomanNumeral):
+            return RomanNumeral(self.__int_representation % int(other))
+        return RomanNumeral(self.__int_representation % int(RomanNumeral(other)))
